8c0bb5b3cf8431e9b19b56023ad17eeac1c887b7
NuoMI <3149441376@qq.com>
2024-05-01 00:06:54 +0800

    项目初始化

---
+++ src/main/java/com/nuomi/staroj/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.nuomi.staroj.controller;
+
+import com.nuomi.staroj.model.dto.postthumb.PostThumbAddRequest;
+import com.nuomi.staroj.common.BaseResponse;
+import com.nuomi.staroj.common.ErrorCode;
+import com.nuomi.staroj.common.ResultUtils;
+import com.nuomi.staroj.exception.BusinessException;
+import com.nuomi.staroj.model.entity.User;
+import com.nuomi.staroj.service.PostThumbService;
+import com.nuomi.staroj.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
